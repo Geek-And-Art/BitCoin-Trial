3f326acf6a00d48f5b8b6ed5a538b7afe92d98ef
Terence Xie <xietao26@gmail.com>
2016-06-22 17:46:22 +0800

    Initial version: add one python file to read CSV file, which is from 'Bitcoin Charts'.

---
+++ src/getTradingData.py
@@ -0,0 +1,23 @@
+'''
+Created on Jun 22, 2016
+
+@author: scntax
+'''
+
+import csv
+import datetime
+
+cr = csv.reader(open('anxhkAUD.csv', 'rb'))
+
+print "time", "price", "amount"
+
+for row in cr:
+    print (datetime.datetime.fromtimestamp(int(row[0])).strftime('%Y-%m-%d %H:%M:%S')), row[1], row[2]
+
+'''
+print(
+    datetime.datetime.fromtimestamp(
+        int(row[0])
+    ).strftime('%Y-%m-%d %H:%M:%S')
+)
+'''
