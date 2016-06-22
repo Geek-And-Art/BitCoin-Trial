9b0ffe46d0519c5a54fd2cf43a32c46347427d95
Terence Xie <xietao26@gmail.com>
2016-06-22 18:27:01 +0800

    Test

--- src/getTradingData.py
+++ src/getTradingData.py
@@ -14,6 +14,7 @@ print "time", "price", "amount"
 for row in cr:
     print (datetime.datetime.fromtimestamp(int(row[0])).strftime('%Y-%m-%d %H:%M:%S')), row[1], row[2]
 
+
 '''
 print(
     datetime.datetime.fromtimestamp(
